518f7d5631ad29803ba398f6214b31d16e13a12f
Martin Medrano <mmedranom@tecnocomfenalco.edu.co>
2026-05-10 18:20:06 -0500

    Implementando las interfaces y repositorios en el serviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Contracts\ProyectoRepositoryInterface;
+use App\Repositories\Contracts\BloqueRepositoryInterface;
+use App\Repositories\Contracts\PiezaRepositoryInterface;
+use App\Repositories\Contracts\RegistroFabricacionRepositoryInterface;
+use App\Repositories\Eloquent\EloquentProyectoRepository;
+use App\Repositories\Eloquent\EloquentBloqueRepository;
+use App\Repositories\Eloquent\EloquentPiezaRepository;
+use App\Repositories\Eloquent\EloquentRegistroFabricacionRepository;
 
-class AppServiceProvider extends ServiceProvider
+class RepositoryServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        $this->app->bind(ProyectoRepositoryInterface::class, EloquentProyectoRepository::class);
+        $this->app->bind(BloqueRepositoryInterface::class, EloquentBloqueRepository::class);
+        $this->app->bind(PiezaRepositoryInterface::class, EloquentPiezaRepository::class);
+        $this->app->bind(RegistroFabricacionRepositoryInterface::class, EloquentRegistroFabricacionRepository::class);
     }
 }
